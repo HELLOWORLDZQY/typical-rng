82133801a99cd8898fc04079f07935f372d2f1cd
XDXDXD <1464689774@qq.com>
2025-08-31 12:07:07 +0800

    Create typical rng.lua

---
+++ typical rng.lua
@@ -0,0 +1,5 @@
+if game.PlaceId == 91694942823334 then
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/HELLOWORLDZQY/typical-rng/refs/heads/main/typical%20rng1.lua", true))()
+else
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/HELLOWORLDZQY/typical-rng/refs/heads/main/typical%20rng2.lua", true))()
+end
